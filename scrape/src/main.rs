55f3f1c088c373f4d7926d195ea328294559b8f3
David Tolnay <dtolnay@gmail.com>
2018-03-17 22:30:50 -0700

    Factor number of versions into length of progress bar

--- scrape/src/main.rs
+++ scrape/src/main.rs
@@ -5,6 +5,7 @@ extern crate rayon;
 use rayon::Scope;
 
 use std::env;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 extern crate indicatif;
 use indicatif::{ProgressBar, ProgressStyle};
@@ -18,7 +19,10 @@ fn main() {
     env::set_var("ALLOW_DOWNLOAD", "");
     let config = rayon::Configuration::new().num_threads(THREADS);
     rayon::initialize(config).unwrap();
-    let pb = ProgressBar::new(total_crates().display_unwrap() as u64);
+
+    let len = total_crates().display_unwrap();
+    let pb = ProgressBar::new(len as u64);
+    let len = AtomicUsize::new(len);
 
     pb.set_style(
         ProgressStyle::default_bar()
@@ -26,37 +30,40 @@ fn main() {
             .progress_chars("&&."),
     );
 
-    rayon::scope(|s| init_index(s, &pb));
+    rayon::scope(|s| init_index(s, &len, &pb));
     pb.finish_and_clear();
 }
 
-fn init_index<'a>(s: &Scope<'a>, pb: &'a ProgressBar) {
+fn init_index<'a>(s: &Scope<'a>, len: &'a AtomicUsize, pb: &'a ProgressBar) {
     let npages = num_pages().display_unwrap();
     for p in 1..npages + 1 {
         s.spawn(move |s| {
             let page = cache_index(p).display_unwrap();
-            init_page(s, &page, pb);
+            init_page(s, &page, len, pb);
         });
     }
 }
 
-fn init_page<'a>(s: &Scope<'a>, page: &IndexPage, pb: &'a ProgressBar) {
+fn init_page<'a>(s: &Scope<'a>, page: &IndexPage, len: &'a AtomicUsize, pb: &'a ProgressBar) {
     for krate in &page.crates {
         let name = krate.name.clone();
         s.spawn(move |s| {
             let k = cache_crate(&name).display_unwrap();
+            let n = k.versions.len();
+            pb.set_length((len.fetch_add(n, Ordering::SeqCst) + n) as u64);
+            init_crate(s, &k, pb);
             pb.inc(1);
-            init_crate(s, &k);
         });
     }
 }
 
-fn init_crate(s: &Scope, k: &Crate) {
+fn init_crate<'a>(s: &Scope<'a>, k: &Crate, pb: &'a ProgressBar) {
     for version in &k.versions {
         let name = k.index.name.clone();
         let num = version.num.clone();
         s.spawn(move |_s| {
             cache_dependencies(&name, &num).display_unwrap();
+            pb.inc(1);
         });
     }
 }
