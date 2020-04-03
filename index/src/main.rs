b893dc2869091b4354a5c2b772c8179cbb7d24a2
David Tolnay <dtolnay@gmail.com>
2020-04-02 22:34:03 -0700

    Update for crates.io-index snapshot-2020-03-25

--- index/src/main.rs
+++ index/src/main.rs
@@ -21,8 +21,9 @@ use std::process;
 
 use crate::error::{Error, Result};
 
-const TIPS: [&str; 3] = [
+const TIPS: [&str; 4] = [
     "origin/master",
+    "origin/snapshot-2020-03-25",
     "origin/snapshot-2019-10-17",
     "origin/snapshot-2018-09-26",
 ];
