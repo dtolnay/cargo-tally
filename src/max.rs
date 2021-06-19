b825c90e2d7ed272efd50795731af3b357da2f83
David Tolnay <dtolnay@gmail.com>
2021-06-19 07:32:17 -0700

    Make extension method for max by key

--- src/max.rs
+++ src/max.rs
@@ -1,17 +1,45 @@
+use crate::hint::TypeHint;
 use crate::present::Present;
+use differential_dataflow::collection::Collection;
 use differential_dataflow::difference::Semigroup;
+use differential_dataflow::lattice::Lattice;
+use differential_dataflow::operators::CountTotal;
+use differential_dataflow::ExchangeData;
 use std::fmt::Debug;
+use std::hash::Hash;
+use std::iter::once;
 use std::ops::{AddAssign, Mul};
+use timely::dataflow::Scope;
+use timely::order::TotalOrder;
 
-#[derive(Clone, Ord, PartialOrd, Eq, PartialEq, Debug)]
-pub struct Max<T> {
-    pub value: T,
+pub(crate) trait MaxByKey<G, K, V, R>
+where
+    G: Scope,
+{
+    fn max_by_key(&self) -> Collection<G, (K, V), isize>;
 }
 
-impl<T> Max<T> {
-    pub fn new(value: T) -> Self {
-        Self { value }
+impl<G, K, V, R> MaxByKey<G, K, V, R> for Collection<G, (K, V), R>
+where
+    G: Scope,
+    K: Clone + ExchangeData + Hash,
+    V: Clone + Ord + ExchangeData + Debug,
+    R: Semigroup,
+    Max<V>: Mul<R, Output = Max<V>>,
+    G::Timestamp: TotalOrder + Lattice,
+{
+    fn max_by_key(&self) -> Collection<G, (K, V), isize> {
+        self.explode(|(key, value)| once((key, Max { value })))
+            .T::<K>()
+            .count_total()
+            .KV::<K, Max<V>>()
+            .map(|(key, max)| (key, max.value))
     }
+}
+
+#[derive(Clone, Ord, PartialOrd, Eq, PartialEq, Debug)]
+pub(crate) struct Max<T> {
+    value: T,
 }
 
 impl<T> Mul<Present> for Max<T> {
