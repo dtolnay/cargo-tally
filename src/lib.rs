97766ccaf77c4806914636d91570e2657fbf564b
David Tolnay <dtolnay@gmail.com>
2024-10-06 22:54:37 +0200

    Ignore needless_lifetimes clippy lint
        warning: the following explicit lifetimes could be elided: 'a
          --> src/matrix.rs:67:6
           |
        67 | impl<'a> DoubleEndedIterator for Iter<'a> {
           |      ^^                               ^^
           |
           = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#needless_lifetimes
           = note: `-W clippy::needless-lifetimes` implied by `-W clippy::all`
           = help: to override `-W clippy::all` add `#[allow(clippy::needless_lifetimes)]`
        help: elide the lifetimes
           |
        67 - impl<'a> DoubleEndedIterator for Iter<'a> {
        67 + impl DoubleEndedIterator for Iter<'_> {
           |
    
        warning: the following explicit lifetimes could be elided: 'a
           --> src/matrix.rs:122:6
            |
        122 | impl<'a> Debug for RelativeRow<'a> {
            |      ^^                        ^^
            |
            = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#needless_lifetimes
        help: elide the lifetimes
            |
        122 - impl<'a> Debug for RelativeRow<'a> {
        122 + impl Debug for RelativeRow<'_> {
            |

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,7 @@
     clippy::missing_panics_doc,
     clippy::module_name_repetitions,
     clippy::must_use_candidate,
+    clippy::needless_lifetimes,
     clippy::needless_pass_by_value,
     clippy::ptr_as_ptr,
     clippy::significant_drop_in_scrutinee,
